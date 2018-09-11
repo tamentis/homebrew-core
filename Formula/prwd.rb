9da619dfd6a3e0afb2fe7a0d637a9b5bddbd9c9e
Bertrand Janin <b@janin.com>
2018-09-11 18:45:47 -0400

    prwd-1.9.1 was released a while back...

--- Formula/prwd.rb
+++ Formula/prwd.rb
@@ -1,9 +1,9 @@
 class Prwd < Formula
   desc "prwd"
   homepage "https://tamentis.com/projects/prwd/"
-  url "https://tamentis.com/projects/prwd/files/prwd-1.9.tar.gz"
-  version "1.9"
-  sha256 "946f7e3761071681e1fac919bb0f3529838917020a0cc38e6449c4432a6d02d8"
+  url "https://tamentis.com/projects/prwd/files/prwd-1.9.1.tar.gz"
+  version "1.9.1"
+  sha256 "ee708dfc5eefb8f23f57acfbbd0601b2fd089fcdb8172459fd3222e2a036fbc7"
 
   def install
     system "./configure", "--prefix=#{prefix}"
