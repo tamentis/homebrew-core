9a66cf53594855e875119d88ed882da2c5dc6b83
Bertrand Janin <b@janin.com>
2018-09-30 12:59:37 -0400

    Add the version we support.

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -5,7 +5,7 @@ class Mdp < Formula
   version "0.7.4"
   sha256 "8f8c3cb4fecea9b4c4fe79aeb4eb7f92a89ae8dd36795f454dae5fc648b7ad12"
 
-  depends_on "gnupg" => :recommended
+  depends_on "gnupg@1.4" => :recommended
 
   def install
     system "./configure", "--prefix=#{prefix}"
