972e1fd484ecca18f01e84e3434486978334c3b2
Bertrand Janin <b@janin.com>
2018-09-30 12:27:06 -0400

    Add gnupg as a recommended dependency.
    It's rather hard to use mdp without gnupg, so assume everyone uses it
    this way, while allowing it turned off.

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -5,6 +5,8 @@ class Mdp < Formula
   version "0.7.4"
   sha256 "8f8c3cb4fecea9b4c4fe79aeb4eb7f92a89ae8dd36795f454dae5fc648b7ad12"
 
+  depends_on "gnupg" => :recommended
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
