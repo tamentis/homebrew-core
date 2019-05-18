06ebd28f9a2a3f6d783876f28aadf08eee34ac82
Bertrand Janin <b@janin.com>
2019-05-18 18:58:57 -0400

    Upgraded ksh to OpenBSD 6.5

--- Formula/openbsd-ksh.rb
+++ Formula/openbsd-ksh.rb
@@ -1,8 +1,8 @@
 class OpenbsdKsh < Formula
   desc "OpenBSD's ksh"
   homepage "https://github.com/tamentis/ksh"
-  url "https://github.com/tamentis/ksh/archive/master.zip"
-  version "6.1"
+  url "https://github.com/tamentis/ksh/archive/6.5.zip"
+  version "6.5"
   sha256 ""
 
   def install
