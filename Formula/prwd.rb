ec5c015790f245ec64b1d472aa31012626ba0113
Bertrand Janin <b@janin.com>
2020-11-16 11:00:17 +0100

    Download sources from Github
    That will save us some bandwidth.

--- Formula/prwd.rb
+++ Formula/prwd.rb
@@ -1,7 +1,7 @@
 class Prwd < Formula
   desc "prwd"
   homepage "https://tamentis.com/projects/prwd/"
-  url "https://tamentis.com/projects/prwd/files/prwd-1.9.2.tar.gz"
+  url "https://github.com/tamentis/prwd/archive/v1.9.2.tar.gz"
   version "1.9.2"
   sha256 "7ebfb1316c0558c79bce0e6499ce94aeb22d2887f79db8b4ca02f4043eb7450f"
 
