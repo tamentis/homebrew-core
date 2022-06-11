616744ba3eee45289aecb35559babd216c86f237
Bertrand Janin <bertrand@janin.com>
2022-06-11 19:27:37 -0400

    Upgrade mdp formula to 0.7.5

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -1,8 +1,8 @@
 class Mdp < Formula
   desc "mdp"
   homepage "https://tamentis.com/projects/mdp/"
-  url "https://tamentis.com/projects/mdp/files/mdp-0.7.4.tar.gz"
-  version "0.7.4"
+  url "https://tamentis.com/projects/mdp/files/mdp-0.7.5.tar.gz"
+  version "0.7.5"
   sha256 "8f8c3cb4fecea9b4c4fe79aeb4eb7f92a89ae8dd36795f454dae5fc648b7ad12"
 
   depends_on "gnupg@1.4" => :recommended
