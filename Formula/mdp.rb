224434cdd9fae0fc093f32249e6231f0aa17c7c2
Bertrand Janin <bertrand@janin.com>
2022-06-11 19:28:56 -0400

    Would be good if I changed the checksum too

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -3,7 +3,7 @@ class Mdp < Formula
   homepage "https://tamentis.com/projects/mdp/"
   url "https://tamentis.com/projects/mdp/files/mdp-0.7.5.tar.gz"
   version "0.7.5"
-  sha256 "8f8c3cb4fecea9b4c4fe79aeb4eb7f92a89ae8dd36795f454dae5fc648b7ad12"
+  sha256 "e6fa117a95ca1c6939766d66ac9c9a1840bf4de1b7180080fc073b04617ed5de"
 
   depends_on "gnupg@1.4" => :recommended
 
